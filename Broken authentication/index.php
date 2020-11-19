a022c14e53a6dab24e07e40c08c5b09069f0d171
kemal yildiz <54835033+Hannibal1461@users.noreply.github.com>
2020-11-19 10:36:48 +0100

    Add files via upload

---
+++ Broken authentication/index.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$email = "email";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $email, $password);
+mysql_select_db("brokenauth");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<html>
+
+  <head>
+    <meta charset="utf-8"
+  </head>
+  <body>
+    <main>
+      <p>log in met je account</p>
+      <form class="contact-form" action="signup.php" method="post">
+      <input type="text" name="username" placeholder="full name">
+      <input type="password" name="password">
+      <button type="submit" name="submit">log in</button>
+      <p><a href="lostpsw.php">password reset</a></p>
+
+      
+
+    </main>
+  </body>
+
+
+</html>
